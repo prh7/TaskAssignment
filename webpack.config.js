7cdc283fded3028e9ebf75d26e3ca86e39f17e00
Praj <prajwalhalwai@protnmail.ch>
2020-11-18 21:07:52 +0100

    added webpack loaders for SCSS/SASS

--- webpack.config.js
+++ webpack.config.js
@@ -67,6 +67,17 @@ const config = {
                     loader: 'html-loader',
                 },
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
         ],
     },
     plugins: [
