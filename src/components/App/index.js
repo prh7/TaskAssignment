b1c033f765b6391409717f5165c12b5349449098
Praj <prajwalhalwai@protnmail.ch>
2020-11-18 20:08:35 +0100

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from 'components/Header';
+
+const App = () => (
+    <>
+        <Header />
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1>Popular Searches</h1>
+                </div>
+            </div>
+        </div>
+    </>
+);
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+    <div className="container">
+        <div className="row">
+            <div className="col">
+                Header
+            </div>
+        </div>
+    </div>
+);
+
+export default Header;
